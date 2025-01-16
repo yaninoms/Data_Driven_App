ed1bdf61d7676a91575a4fad1a7f2a000803d507
yaninoms <144207242+yaninoms@users.noreply.github.com>
2025-01-16 19:31:00 +0400

    country_quiz

--- ospath.py
+++
@@ -1,9 +0,0 @@
-import os
-
-map_file = "map.html"
-full_path = os.path.abspath(map_file)
-print(f"Map file saved at: {full_path}")  # Debugging
-if os.path.exists(full_path):
-    self.map_frame.load_url(f"file://{full_path}")
-else:
-    print("Error: Map file not found!")
